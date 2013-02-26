2d2bba85c0496060ef71419d6f45ca9c50ec5302
Daniel Savage <dansvg@gmail.com>
2013-02-26 15:02:16 -0800

    added class files for cleint and server
    no functionality at all yet

---
+++ RTPClient.java
@@ -0,0 +1,10 @@
+import java.net.*;
+import javax.media.rtp.*;
+
+public class RTPClient
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello, I am client!");
+	}
+} //RTPClient
---
+++ RTPServer.java
@@ -0,0 +1,10 @@
+import java.net.*;
+import javax.media.rtp.*;
+
+public class RTPServer
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello, I am client!");
+	}
+} //RTPServer
